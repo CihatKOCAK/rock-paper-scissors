311d3cc5062f5d7cb5b8bd490ca873d9da7a9f23
Cihat KOÇAK <pcihatkocakks@gmail.com>
2021-12-21 20:51:31 +0300

    ui-ok

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,14 +4,16 @@ import "./menu.scss";
 export default function menu({ setSelectedMenu }) {
 
     return (
-        <form action="/game">
-            <div className='containerMenu' >
-                <h1 className='menuH'>MENU</h1>
-                <hr />
-                <button onClick={() => setSelectedMenu("Resume")}><span>Resume</span></button>
-                <button onClick={() => setSelectedMenu("NewGame")}>New Game</button>
-                <button onClick={() => setSelectedMenu("Start")} >Start</button>
-            </div >
-        </form>
+       
+            <form className='actionForm' action="/game">
+                <div className='containerMenu' >
+                    <h1 className='menuH'>MENU</h1>
+                    <hr />
+                    <button onClick={() => setSelectedMenu("Resume")}><span>Resume</span></button>
+                    <button onClick={() => setSelectedMenu("NewGame")}>New Game</button>
+                    <button onClick={() => setSelectedMenu("Start")} >Start</button>
+                </div >
+            </form>
+       
     )
 }
