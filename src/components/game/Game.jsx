50fd8cd5ed6010bdb7c8046021dff422d4e11848
Cihat KOÇAK <pcihatkocakks@gmail.com>
2021-12-22 18:47:17 +0300

    dialerOK

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -8,21 +8,32 @@ import Head from './head/Head';
 export default function Game({ setLoggined, data, setData }) {
 
   const [userSelection, setUserSelection] = useState("assets/qmark.png")
-  
   const [pcSelection, setPcSelection] = useState("assets/qmark.png")
-
 
   useEffect(() => {
     var selections = ["assets/rock.png", "assets/paper.png", "assets/scissors.png"]
     setPcSelection(selections[Math.floor(Math.random() * selections.length)]);
   }, [userSelection])
 
-  console.log("user :",userSelection, "pc :",pcSelection)
+  const selectionDialer = (s) => {
+    var v1 = s.split('/');
+    var v2 = v1[1].split('.');
+    var selection = v2[0];
+    return selection;
+  }
+
+  
+
+  console.log("user :", selectionDialer(userSelection), "pc :", selectionDialer(pcSelection))
+
   return (
     <div className='game'>
       <Head setLoggined={setLoggined} />
       <br />
-      <Container credit={data[0].credit} userSelection={userSelection} pcSelection={pcSelection} />
+      <Container
+        credit={data[0].credit}
+        userSelection={userSelection}
+        pcSelection={pcSelection} />
       <Bottom setUserSelection={setUserSelection} />
     </div>
   )
